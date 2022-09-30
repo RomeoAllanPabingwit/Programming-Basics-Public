60287cd0b411a711043a270492b43e77dcf762ea
Romeo Allan Pabingwit <jrpabs510@gmail.com>
2022-09-29 18:07:38 -0700

    Add files via upload

---
+++ Week1/Day5/CodeChallenge_LoopingThroughArrays/LoopingThroughArrays.js
@@ -0,0 +1,25 @@
+// #1
+var arr1 = [8, 6, 7, 5, 3, 0, 9];
+    for (var x = 0; x <= arr1.length-1; x++){    
+    console.log(arr1[x]);
+    }
+
+// #2
+var arr2 = [4, 7, 13, 13, 19, 37, -2];
+    for (var x = 0; x <= arr2.length-1; x++){
+    console.log (arr2[x] + x);
+    console.log(arr2[x]);
+    } 
+
+// #3
+var arr3 = [6, 2, 12, 14, -24, 5, 0];
+    for (x = 0; x <= arr3.length-1; x++)
+    if (arr3[x] > 5){
+        console.log(arr3[x]);
+    }
+        
+        
+
+   
+    
+
---
+++ Week1/Day5/CodeChallenge_PredictTheOutput/PredictTheOutput.js
@@ -0,0 +1,81 @@
+// ----Challenge 1----
+
+var arr = ["Up", "You", "Give", "To", "Going", "Never"];
+for(var i = arr.length-1; i >= 0; i = i - 1) {
+    console.log(arr[i]);
+}
+
+/*  T-Diagram
+
+var         |        value
+---------------------------------
+arr         |  ["Up", "You", "Give", "To", "Going", "Never"]
+i           |  5 > 4 > 3 > 2 > 1 > 0
+
+console:
+Never
+Going
+To
+Give
+You
+Up
+
+*/
+
+// ----Challenge 2----
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var sum = 0;
+for(var i = 1; i < arr.length; i = i + 2) {
+    sum = sum + arr[i];
+    console.log(arr[i]);
+    console.log(sum);
+}
+
+/*  T-Diagram
+
+var         |        value
+---------------------------------
+arr         | [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]   10 = arr.lenght
+i           | 1 > 3 > 5 > 7 > 9 > 11 not less than array lenght
+sum         | 0 > 2 > 4 > 6 > 8 > 10 
+
+console:
+2
+2
+4
+6
+6
+8
+20
+10
+30
+
+
+
+*/
+
+// ----Challenge 3----
+
+var arr = [8, 2, 0, 6, 12, 4, 3];
+for(var i = 0; i < arr.length; i++){
+    if(arr[i] >= 6) {
+        console.log(true);
+    }
+}
+
+/*  T-Diagram
+
+var         |        value
+---------------------------------
+arr         | [8, 2, 0, 6, 12, 4, 3]  7 = arrey lenght
+i           | 0 > 1 > 2 > 3 > 4 > 5 > 6 > 7 not less than array lenght
+
+
+console:
+true
+true
+true
+
+
+*/
